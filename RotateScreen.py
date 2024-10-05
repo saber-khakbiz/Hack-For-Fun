d74f1323b820186c379d4f08a87a3eaed3f4b18a
Saber Khakbiz <83659641+saber-khakbiz@users.noreply.github.com>
2024-10-05 11:40:43 +0330

    Update RotateScreen.py
    + Fix Bugs

--- RotateScreen.py
+++ RotateScreen.py
@@ -1,12 +1,21 @@
-import  time
+import time
+import platform
 
-import rotatescreen as rs
+try:
+    import rotatescreen as rs
+except ImportError:
+    rs = None
 
-display = rs.get_primary_display()
+# شناسایی سیستم‌عامل
+os_name = platform.system()
 
-listOfAngels= [90, 180, 270, 0]
+if os_name == 'Windows' and rs:
+    display = rs.get_primary_display()
+    listOfAngles = [90, 180, 270, 0]
 
-while True:
-    for x in listOfAngels:
-        display.rotate_to(x)
-        time.sleep(1)
+    while True:
+        for angle in listOfAngles:
+            display.rotate_to(angle)
+            time.sleep(3)
+else:
+    print(f"Screen rotation is not supported on {os_name}.")
